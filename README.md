e32c9e10d40209254a8b90a54eddc1a2dafaf55e
Martin Kase <martinkase@Martins-MacBook-Air.local>
2015-12-05 22:34:38 +0100

    Update

---
+++ README.md
@@ -0,0 +1,20 @@
+# README
+## This is the README for extension PHP Format intend
+
+* PHP Formator for VS Code using native VS Code format system.
+* Formator use basic rules of indent format system.
+* You can use classic format shortcut or 'format' button in menu.
+* Now its WORKS with PHP and HTML in one file!
+
+### Bug
+* If you found bug or you have an idea, let me now on GitHub :)
+* https://github.com/kasik96/VSCode-Indent-Format
+
+### Thanks to
+* UW CSE Indenter Tool - http://goo.gl/FWGCha
+* Microsoft - https://www.microsoft.com/
+* Sean McBreen 
+* js-beautify - https://www.npmjs.com/package/js-beautify
+
+
+**Made with ♥ and VSCode. Enjoy!**
---
+++ extension.js
@@ -0,0 +1,138 @@
+var vscode = require('vscode');
+var beautify = require('js-beautify').html;
+
+function format(document, range, options) {
+    if (range === null) {
+        var start = new vscode.Position(0, 0);
+        var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+        range = new vscode.Range(start, end);
+    }
+    var result = [];
+    var content = document.getText(range);
+    var formatted = Indentation(content);
+	var newFormatted = beautify(formatted, { indent_size: 2 });
+	
+    if (newFormatted) {
+        result.push(new vscode.TextEdit(range, newFormatted));
+    }
+    return result;
+}
+
+function Indentation(oString) {
+    var indentString = "    ";
+    var code = oString;
+    var newCode = "";
+    var indentLevel = 0;
+    var indentDuration = 0;
+    var newLines = [];
+    var inMultiLineComment = false;
+    var lines = code.split(/[\r]?\n/gi);
+    for (var i = 0; i < lines.length; i++) {
+        var line = trim(lines[i]);
+        var lineForMatching = line.replace(/\/\/.*/, "");
+        if (inMultiLineComment) {
+            if (line.match(/\*\//)) {
+                lineForMatching = line.replace(/.*\*\//, "");
+                inMultiLineComment = false;
+            }
+            else {
+                lineForMatching = "";
+            }
+        }
+        if (lineForMatching.match(/\/\*/)) {
+            if (lineForMatching.match(/\*\//)) {
+                lineForMatching = lineForMatching.replace(/\/\*.*\*\//, "");
+            }
+            else {
+                inMultiLineComment = true;
+                lineForMatching = lineForMatching.replace(/\/\*.*/, "");
+            }
+        }
+        var lbrackets = lineForMatching.replace(/[^\{]+/gi, "");
+        var rbrackets = lineForMatching.replace(/[^\}]+/gi, "");
+        var lbracket1 = lineForMatching.indexOf("{");
+        var rbracket1 = lineForMatching.indexOf("}");
+        var lbracketN = lineForMatching.lastIndexOf("{");
+        var rbracketN = lineForMatching.lastIndexOf("}");
+        var increaseIndentBefore = false;
+        var decreaseIndentBefore = false;
+        var increaseIndentAfter = false;
+        var decreaseIndentAfter = false;
+        if (lbrackets.length > rbrackets.length ||
+            lbracketN >= 0 && lbracketN > rbracketN) {
+            increaseIndentAfter = true;
+        }
+        if (rbrackets.length > lbrackets.length ||
+            rbracket1 >= 0 && rbracket1 < lbracket1) {
+            decreaseIndentBefore = true;
+        }
+        if (indentDuration > 0) {
+            indentDuration--;
+            if (trim(lineForMatching).indexOf("{") >= 0) {
+                decreaseIndentBefore = true;
+            }
+            else if (indentDuration == 0) {
+                decreaseIndentAfter = true;
+            }
+        }
+        if ((((lbrackets.length == 0 && rbrackets.length == 0))
+            &&
+			((lineForMatching.match(/(if |while )[ \t]*([^)]*)/) && !lineForMatching.match(/;/)) ||
+				(lineForMatching.match(/(for )[ \t]*([^)]*)/)) ||
+				(lineForMatching.match(/else/) &&
+					(!lineForMatching.match(/else[ ]+if/) &&
+						(lbrackets.length == 0 || lbrackets.length > rbrackets.length)))))
+            || trim(lineForMatching).match(/}[ \t]*else$/)) {
+            increaseIndentAfter = true;
+            indentDuration = 1;
+        }
+        if (increaseIndentBefore) {
+            indentLevel++;
+        }
+        else if (decreaseIndentBefore) {
+            indentLevel = Math.max(0, indentLevel - 1);
+        }
+        for (var tabs = 0; tabs < indentLevel; tabs++) {
+            line = indentString + line;
+        }
+        newLines.push(line);
+        if (increaseIndentAfter) {
+            indentLevel++;
+        }
+        else if (decreaseIndentAfter) {
+            indentLevel = Math.max(0, indentLevel - 1);
+        }
+    }
+    newCode = newLines.join("\n");
+    return newCode;
+}
+exports.Indentation = Indentation;
+function ltrim(str) {
+    for (var k = 0; k < str.length && str.charAt(k) <= " "; k++)
+        ;
+    return str.substring(k, str.length);
+}
+function rtrim(str) {
+    for (var j = str.length - 1; j >= 0 && str.charAt(j) <= " "; j--)
+        ;
+    return str.substring(0, j + 1);
+}
+function trim(str) {
+    return ltrim(rtrim(str));
+}
+
+function activate(context) {
+	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('php', {
+        provideDocumentFormattingEdits: function (document, options, token) {
+            return format(document, null, options);
+        }
+    }));
+    context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider('php', {
+        provideDocumentRangeFormattingEdits: function (document, range, options, token) {
+            var start = new vscode.Position(0, 0);
+            var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+            return format(document, new vscode.Range(start, end), options);
+        }
+    }));
+}
+exports.activate = activate;
